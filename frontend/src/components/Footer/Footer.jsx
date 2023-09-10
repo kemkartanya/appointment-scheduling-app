60ee27b48923ba562bd6095653fd29c567a017a6
Tanya Kemkar <tanyakemkar@gmail.com>
2023-09-11 02:50:38 +0530

    frontend

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -1,8 +1,157 @@
 import React from 'react'
 
+import {Link} from 'react-router-dom'
+import logo from '../../assets/images/logo.png'
+import {RiLinkedinFill} from 'react-icons/fi'
+import { AiFillYoutube, AiFillGithub, AiOutlineInstagram } from 'react-icons/ai'
+
+const socialLinks = [
+  {
+    path: "https://www.youtube.com/c/tanyakemkar",
+    icon: <AiFillYoutube className='group-hover:text-white w-4 h-5' />
+  },
+  {
+    path: "https://www.github.com/kemkartanya",
+    icon: <AiFillGithub className='group-hover:text-white w-4 h-5' />
+  },
+  {
+    path: "https://www.instagram.com/tanyakemkar",
+    icon: <AiOutlineInstagram className='group-hover:text-white w-4 h-5' />
+  },
+  {
+    path: "https://www.linkedin.com/in/tanyakemkar",
+    icon: <RiLinkedinFill className='group-hover:text-white w-4 h-5' />
+  },
+];
+
+const quickLinks01 = [
+  {
+    path: "/home",
+    display: "Home",
+  },
+  {
+    path: "/",
+    display: "About Us",
+  },
+  {
+    path: "/services",
+    display: "Services",
+  },
+  {
+    path: "/",
+    display: "Blog",
+  },
+]
+
+const quickLinks02 = [
+  {
+    path: "/home",
+    display: "Home",
+  },
+  {
+    path: "/",
+    display: "About Us",
+  },
+  {
+    path: "/services",
+    display: "Services",
+  },
+  {
+    path: "/",
+    display: "Blog",
+  },
+]
+
+const quickLinks03 = [
+  {
+    path: "/home",
+    display: "Home",
+  },
+  {
+    path: "/",
+    display: "About Us",
+  },
+]
+
 const Footer = () => {
+
+  const year = new Date().getFullYear()
+
   return (
-    <div>Footer</div>
+    <footer>
+      <div className="container">
+        <div className='flex justify-between flex-col md:flex-row flex-wrap gap-[30px]'>
+          <div>
+            <img src={logo} alt='' />
+            <p className='text-[16px] leading-7 font-[400] text-textColor mt-4'>
+              Copyright @ {year} developed by Tanya Kemkar all rights reserved.
+            </p>
+
+            <div>
+              {socialLinks.map((link, index) => (
+                <Link
+                  to={link.path}
+                  key={index}
+                  className='w-9 h-9 border border-solid border-[#181A1E] rounded-full flex items-center
+                  justify-center group hover:bg-primaryColor hover:border-none'
+                >
+                {link.icon}
+                </Link>
+              ))}
+            </div>
+          </div>
+
+          <div>
+            <h2 className='text-[20px] leading-[30px] font-[700] mb-6 text-headingColor'>Quick Links</h2>
+
+            <ul>
+              {quickLinks01.map((item, index) => (
+                <li key={index} className='mb-4'>
+                  <LinK 
+                  to={item.path} 
+                  className="text-[16px] leading-7 font-[400] text-textColor">
+                    {item.display}
+                  </LinK>
+                </li>
+              ))}
+            </ul>
+          </div>
+
+          <div>
+            <h2 className='text-[20px] leading-[30px] font-[700] mb-6 text-headingColor'>I want to </h2>
+
+            <ul>
+              {quickLinks02.map((item, index) => (
+                <li key={index} className='mb-4'>
+                  <LinK 
+                  to={item.path} 
+                  className="text-[16px] leading-7 font-[400] text-textColor">
+                    {item.display}
+                  </LinK>
+                </li>
+              ))}
+            </ul>
+          </div>
+
+          <div>
+            <h2 className='text-[20px] leading-[30px] font-[700] mb-6 text-headingColor'>Support</h2>
+
+            <ul>
+              {quickLinks03.map((item, index) => (
+                <li key={index} className='mb-4'>
+                  <LinK 
+                  to={item.path} 
+                  className="text-[16px] leading-7 font-[400] text-textColor">
+                    {item.display}
+                  </LinK>
+                </li>
+              ))}
+            </ul>
+          </div>
+
+        </div>
+      </div>
+    </footer>
   )
 }
 
--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -2,6 +2,7 @@ import {useEffect, useRef} from 'react'
 import logo from '../../assets/images/logo.png'
 import userImg from '../../assets/images/avatar-icon.png'
 import {NavLink, Link} from 'react-router-dom' 
+import {BiMenu} from 'react-icons/bi'
 
 const navLinks = [
   {
@@ -23,8 +24,31 @@ const navLinks = [
 ]
 
 const Header = () => {
+
+  const headerRef = useRef(null)
+  const menuRef = useRef(null)
+
+
+  const handleStickyHeader = () => {
+    window.addEventListener('scroll', ()=>{
+      if(document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+        headerRef.current.classList.add('sticky__header');
+      } else {
+        headerRef.current.classList.remove('sticky__header');
+      }
+    })
+  }
+
+  useEffect(()=>{
+    handleStickyHeader()
+
+    return ()=> window.removeEventListener('scroll', handleStickyHeader)
+  })
+
+  const toggleMenu = ()=> menuRef.current.classList.toggle('show__menu')
+
   return (
-    <header className='header flex items-center'>
+    <header className='header flex items-center' ref={headerRef}>
       <div className="container">
         <div className='flex items-center justify-between'>
           {/* =================== logo =================== */}
@@ -33,7 +57,7 @@ const Header = () => {
           </div>
 
           {/* =================== menu =================== */}
-          <div className='navigation'>
+          <div className='navigation' ref={menuRef} onClick={toggleMenu}>
             <ul className='menu flex items-center gap-[2.7rem]'>
               {navLinks.map((link, index) => (
                 <NavLink
@@ -53,13 +77,21 @@ const Header = () => {
           {/* =================== nav right =================== */}
           <div className='flex items-center gap-4'>
 
-            <div>
+            <div className='hidden'>
               <Link to='/'>
-                <figure className='w-[35px] h-[35px] rounded-full'>
+                <figure className='w-[35px] h-[35px] rounded-full cursor-pointer'>
                   <img className='w-full rounded-full' src={userImg} alt='' />
                 </figure>
               </Link>
             </div>
+
+            <Link to='/login'>
+              <button className='bg-primaryColor py-2 px-6 text-white font-[600] h-[44px] flex items-center justify-center rounded-[50px]'>Login</button>
+            </Link>
+
+            <span className='md:hidden' onClick={toggleMenu}>
+              <BiMenu className="w-6 h-6 cursor-pointer" />
+            </span>
 
           </div>
         </div>
