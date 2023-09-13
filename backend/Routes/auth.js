96b3b963674370a82f8325a4307bb088021e95ba
Tanya Kemkar <tanyakemkar@gmail.com>
2023-09-13 16:37:05 +0530

    backend apis

--- backend/Routes/auth.js
+++ backend/Routes/auth.js
@@ -1,7 +1,7 @@
 
 
 import express from 'express'
-import {register, login } from '../Controllers/authControllers.js'
+import {register, login } from '../Controllers/authController.js'
 
 const router = express.Router();
 
